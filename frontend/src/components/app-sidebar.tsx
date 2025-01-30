6e0fb3d91796e645a9e3bf8eab353dd6a738a390
Diogo Stahnke <diogostk.br@gmail.com>
2025-01-30 10:29:26 -0300

    Adiciona barra lateral para navegação

---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,67 @@
+import { GalleryVerticalEnd } from "lucide-react";
+import * as React from "react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail
+} from "@/components/ui/sidebar";
+
+const data = {
+  navMain: [
+    {
+      title: "Pedidos",
+      url: "#",
+    },
+    {
+      title: "Produtos",
+      url: "#",
+    },
+    {
+      title: "Ordem de Transporte",
+      url: "#",
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="flex aspect-square size-5 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
+                  <GalleryVerticalEnd className="size-3" />
+                </div>
+                <span className="font-semibold">Pedidos App</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className="font-medium">
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
