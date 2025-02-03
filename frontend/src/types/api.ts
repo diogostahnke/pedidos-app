091312687002b4ebc34a9553d3444c17c5ec54fb
Diogo Stahnke <diogostk.br@gmail.com>
2025-02-03 16:52:43 -0300

    Cria e configura o objeto api

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -1,5 +1,16 @@
 export interface Produto {
   id: number;
   titulo: string;
-  preco: string;
+  preco: number;
+}
+
+export interface Pedido {
+  id: number;
+  valor: string;
+  criado_em: string;
+}
+
+export interface PedidoRequest {
+  valor: string;
+  produtos: Produto["id"][];
 }
